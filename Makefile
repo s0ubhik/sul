a03f3152a14a65c99b343dfd9ca530e1f009fec3
s0ubhik <soubhik1809@gmail.com>
2023-09-09 23:12:37 +0530

    changed Makefile

--- Makefile
+++ Makefile
@@ -1,22 +1,26 @@
+ifdef NOPASS
+	DEPS = main.o
+	DEF =
+else
+	DEPS = main.o md5-c/md5.o
+	DEF = -DPASSWORD
+endif
+
 %.o: %.c
-	$(CC) -c $< -o $@
+	$(CC) $(DEF) -c $< -o $@
 
-all: sulock
-
-main_wp.c: main_wp.patch
-	@echo "Configuring without password..."
-	cp main.c main_wp.c
-	patch main_wp.c < main_wp.patch
-
-sul: main_wp.o
-	$(CC) $^ -o $@
-	sudo su -c 'chown root:root $@; chmod 4111 $@'
-
-sulock: main.o md5-c/md5.o
+sul: $(DEPS)
 	$(CC) $^ -o $@
 	sudo su -c 'chown root:root $@; chmod 4111 $@'
 # 4111 - user(s) group(x) others(x)
 
+install: sul
+	sudo cp $< /usr/bin
+	sudo su -c 'chown root:root $<; chmod 4111 $<'
+
+uninstall: /usr/bin/sul
+	sudo rm -f $<
 clean:
-	rm -rf *.o md5-c/*.o main_wp.c
-	sudo su -c 'rm -f sulock sul'
+	rm -rf *.o md5-c/*.o
+	sudo bash -c 'rm -f sul'
+
--- main.c
+++ main.c
@@ -3,14 +3,24 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "md5-c/md5.h"
 
+#ifdef PASSWORD
+    #define ARG_CMD 2
+    #include "md5-c/md5.h"
+#else
+    #define ARG_CMD 1
+#endif
 
 int print_help(){
+#ifdef PASSWORD
     printf("Usage: sulock [PASSWORD] [COMMAND]\n");
+#else
+    printf("Usage: sulock [COMMAND]\n");
+#endif
     return 0;
 }
 
+#ifdef PASSWORD
 int check_pswd(uint8_t pswd[16]){
     uint8_t password[16] = {0x81, 0xdc, 0x9b, 0xdb, 0x52, 0xd0, 0x4d, 0xc2, 0x00, 0x36, 0xdb, 0xd8, 0x31, 0x3e, 0xd0, 0x55};
     for(unsigned int i = 0; i < 16; ++i)
@@ -24,20 +34,25 @@ void print_hash(uint8_t *p){
     }
     printf("\n");
 }
+#endif
 
 int main(int argc, char* argv[]){
-    if (argc < 3 || strcmp(argv[argc-1], "-h") == 0) return print_help();
+    if (argc < ARG_CMD+1 || strcmp(argv[argc-1], "-h") == 0) return print_help();
 
+    /* check password */
+#ifdef PASSWORD
     uint8_t pswd[16];
     md5String(argv[1], pswd);
     if (!check_pswd(pswd)) {
         printf("Permssion Denied!\n");
         return(1);
     }
+#endif
 
-    char* cmd = malloc(strlen(argv[2]) * sizeof(char));
-    strcpy(cmd, argv[2]);
-    for (int i = 3; i < argc; i++){
+    /* make command string */
+    char* cmd = malloc(strlen(argv[ARG_CMD]) * sizeof(char));
+    strcpy(cmd, argv[ARG_CMD]);
+    for (int i = ARG_CMD+1; i < argc; i++){
        int s = strlen(argv[i]) + strlen(cmd) + 1;
        if (s > 256) cmd = realloc(cmd, s*sizeof(char));
        strcat(cmd, " ");
